be5d9753626b30173dec6fd4e007a3cf89ecc50f
Yejin1 <dpwls273273@gmail.com>
2025-09-16 23:25:30 +0900

    (오류개선) 빌드 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
+};
+
+module.exports = {
+  env: {
+    PORT: 8080,
+  },
 };
 
 export default nextConfig;
