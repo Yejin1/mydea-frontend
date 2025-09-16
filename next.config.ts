557ab07bea37d1c4a011faf1ae03cc9dbbbf5c7a
Yejin1 <dpwls273273@gmail.com>
2025-09-17 02:24:54 +0900

    (오류개선) 빌드 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
+// next.config.ts (ESM)
 import type { NextConfig } from "next";
-
-const config: NextConfig = {
-  output: "standalone",
-};
-
+const config: NextConfig = { output: "standalone" };
 export default config;
